88716fe683b943f73a27d161461623aa2bbc035c
naczea <nico.acz21@gmail.com>
2022-01-27 04:46:05 -0500

    server change

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://34.72.242.45:3050';
+axios.defaults.baseURL = 'https://cryptovision-api.herokuapp.com/';
 
 ReactDOM.render(
   <React.Fragment>
